ff9bc5d72fd7a67e731fd5e2e9ebedbb6a43ee6f
James Farmer <james.farmer@starlingbank.com>
2024-12-03 18:04:25 +0000

    AOC 2024 - Day 3 - PowerShell/Bash Solutions

--- 031224/031224-Part2.sh
+++ 031224/031224-Part2.sh
@@ -1,4 +1,3 @@
 # https://adventofcode.com/2024/day/3
-# This script did not answer the challenge correctly.
 
 paste -s corruptedMemory.txt | sed -E "s/don't/\ndon't/g" | sed -E "s/do\(\)/\ndo\(\)/g" | grep -v "^don" | sed -E 's/mul/\nmul/g' | sed -nE 's/.*mul\(([0-9]+),([0-9]+)\).*/\1\*\2/p' | paste -sd+ - | bc
