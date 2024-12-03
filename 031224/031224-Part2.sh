4534a3d83ca7f598a04e1c0438a0ce5521590211
James Farmer <jjhfarmer@gmail.com>
2024-12-03 17:55:54 +0000

    remove previous solutions

--- 031224/031224-Part2.sh
+++
@@ -1,3 +0,0 @@
-# https://adventofcode.com/2024/day/3
-
-paste -s corruptedMemory.txt | sed -E "s/don't/\ndon't/g" | sed -E "s/do\(\)/\ndo\(\)/g" | grep -v "^don" | sed -E 's/mul/\nmul/g' | sed -nE 's/.*mul\(([0-9]+),([0-9]+)\).*/\1\*\2/p' | paste -sd+ - | bc
